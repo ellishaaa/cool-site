2f28fef72d047b7c4912583515bed29df2435f5e
Ellisha Webur <ejwebur@gmail.com>
2024-01-15 16:01:12 -0600

    removing warnings

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ useEffect(() => {
       <nav className='navbar'>
         <div className='navbar-container'>
             <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-                Whoopie&nbsp;<img className='nav-img' src='../images/WhoopieCushion.png'/>
+                Whoopie&nbsp;<img alt='logo' className='nav-img' src='../images/WhoopieCushion.png'/>
             </Link>
             <div className='menu-icon' onClick={handleClick}>
                 <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
--- src/components/Pages/SignUp.js
+++ src/components/Pages/SignUp.js
@@ -1,6 +1,5 @@
 import React from "react";
 import '../../App.css';
-import { Button } from "../Button";
 import EmailForm from "../EmailForm";
 
 export default function SignUP(){
