62384645ab2e8481f4f0e2a03bb5cbbdaf9fa0d5
Ellisha Webur <ejwebur@gmail.com>
2024-01-28 21:05:31 -0600

    testing

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,8 +7,8 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       <video src="/videos/video-2.mp4" autoPlay loop muted />
-      <h1>Adventure is badass!</h1>
-      <p>Let's gooo</p>
+      <h1>Test site</h1>
+      <p>Let's gooooooo!</p>
 
       <div className='hero-btns'>
         <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>Get started</Button>
