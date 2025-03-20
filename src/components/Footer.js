08fda4df0ddc4bd148204351f0b237c0c4951d5a
ellishaaa <ejwebur@gmail.com>
2025-03-20 04:49:31 -0500

    Hmmmm

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -28,25 +28,7 @@ function Footer() {
               </form>
             </div>
           </section>
-          {/* <div className='footer-links'>
-            <div className='footer-link-wrapper'>
-              <div className='footer-link-items'>
-                <h2>About Us</h2>
-                <Link to='/sign-up'>How it works</Link>
-                <Link to='/'>Testimonials</Link>
-                <Link to='/'>Careers</Link>
-                <Link to='/'>Investors</Link>
-                <Link to='/'>Terms of Service</Link>
-              </div>
-              <div className='footer-link-items'>
-                <h2>Contact Us</h2>
-                <Link to='/'>Contact</Link>
-                <Link to='/'>Support</Link>
-                <Link to='/'>Destinations</Link>
-                <Link to='/'>Sponsorships</Link>
-              </div>
-            </div>
-          </div> */}
+
           <section className='social-media'>
             <div className='social-media-wrap'>
               <div className='footer-logo'>
