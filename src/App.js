b73e5fc1c95b8f79b5055b3d3f6ce23e92856ed6
Ellisha Webur <ejwebur@gmail.com>
2025-12-15 11:56:44 -0600

    Scroll to top feature

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Projects from "./components/Pages/Products";
 import About from "./components/Pages/Services";
 import SignUP from "./components/Pages/SignUp";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
       <Router>
+        <ScrollToTop />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
